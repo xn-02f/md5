55c9387a1f6ac5762332c8c394f5c81c240e5041
huiyifyj <jxfengyijie@gmail.com>
2018-11-10 16:34:05 +0800

    Upgrade to arrow function for es6

--- test.js
+++ test.js
@@ -19,17 +19,17 @@ const md5Hash = {
     url: '014dab5b5a990d379b2306f5d0839261'
 }
 
-describe('MD5 Test', function () {
+describe('MD5 Test', () => {
 
-    it('"' + example.name + '" Converted to MD5 Value Test.', function () {
+    it('"' + example.name + '" Converted to MD5 Value Test.', () => {
         assert.equal(md5(example.name), md5Hash.name);
     });
 
-    it('"' + example.email + '" Converted to MD5 Value Test.', function () {
+    it('"' + example.email + '" Converted to MD5 Value Test.', () => {
         assert.equal(md5(example.email), md5Hash.email);
     });
 
-    it('"' + example.url + '" Converted to MD5 Value Test.', function () {
+    it('"' + example.url + '" Converted to MD5 Value Test.', () => {
         assert.equal(md5(example.url), md5Hash.url);
     });
 
