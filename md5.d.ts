e338edd51bdf29056ba81ef2209c5cbeb0dba345
pshif <425930357@qq.com>
2019-02-16 19:33:29 +0800

    Create type declaration file to support ts
    - Create type declaration file(md5.d.ts) file to support typescript.
    - Add `types` word in package.json file.

---
+++ md5.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Convert string to md5 hash.
+ *
+ * @param {string} value - The raw string you want to convert.
+ * @return {string} The hexadecimal hash string that is handled and converted.
+ */
+export = md5;
+
+declare const md5: (value: string) => string;
