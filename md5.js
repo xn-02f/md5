01cda158c4108d2be2e9abf43d45365e52d87431
huiyifyj <jxfengyijie@gmail.com>
2019-03-30 15:12:30 +0800

    Add explanatory notes and rename functions
    - Rename md5ff, md5gg, md5hh, md5ii functions to FF, GG, HH, II.
    - Add some detailed comments for bitRotateLeft and FF,GG,HH,II functions.

--- md5.js
+++ md5.js
@@ -48,6 +48,9 @@ const safeAdd = (x, y) => {
     return (msw << 16) | (lsw & 0xffff);
 }
 
+/**
+ * bitRotateLeft rotates x left n bits.
+ */
 const bitRotateLeft = (num, cnt) => {
     return (num << cnt) | (num >>> (32 - cnt));
 }
@@ -55,16 +58,21 @@ const bitRotateLeft = (num, cnt) => {
 const md5cmn = (q, a, b, x, s, t) => {
     return safeAdd(bitRotateLeft(safeAdd(safeAdd(a, q), safeAdd(x, t)), s), b);
 }
-const md5ff = (a, b, c, d, x, s, t) => {
+
+/**
+ * FF, GG, HH, and II transformations for rounds 1, 2, 3, and 4.
+ * Rotation is separate from addition to prevent recomputation.
+ */
+const FF = (a, b, c, d, x, s, t) => {
     return md5cmn((b & c) | (~b & d), a, b, x, s, t);
 }
-const md5gg = (a, b, c, d, x, s, t) => {
+const GG = (a, b, c, d, x, s, t) => {
     return md5cmn((b & d) | (c & ~d), a, b, x, s, t);
 }
-const md5hh = (a, b, c, d, x, s, t) => {
+const HH = (a, b, c, d, x, s, t) => {
     return md5cmn(b ^ c ^ d, a, b, x, s, t);
 }
-const md5ii = (a, b, c, d, x, s, t) => {
+const II = (a, b, c, d, x, s, t) => {
     return md5cmn(c ^ (b | ~d), a, b, x, s, t);
 }
 
@@ -86,73 +94,73 @@ const binlMD5 = (x, len) => {
 
     for (let i = 0; i < x.length; i += 16) {
         /* Round 1 */
-        a = md5ff(a, b, c, d, x[i], 7, -680876936);
-        d = md5ff(d, a, b, c, x[i + 1], 12, -389564586);
-        c = md5ff(c, d, a, b, x[i + 2], 17, 606105819);
-        b = md5ff(b, c, d, a, x[i + 3], 22, -1044525330);
-        a = md5ff(a, b, c, d, x[i + 4], 7, -176418897);
-        d = md5ff(d, a, b, c, x[i + 5], 12, 1200080426);
-        c = md5ff(c, d, a, b, x[i + 6], 17, -1473231341);
-        b = md5ff(b, c, d, a, x[i + 7], 22, -45705983);
-        a = md5ff(a, b, c, d, x[i + 8], 7, 1770035416);
-        d = md5ff(d, a, b, c, x[i + 9], 12, -1958414417);
-        c = md5ff(c, d, a, b, x[i + 10], 17, -42063);
-        b = md5ff(b, c, d, a, x[i + 11], 22, -1990404162);
-        a = md5ff(a, b, c, d, x[i + 12], 7, 1804603682);
-        d = md5ff(d, a, b, c, x[i + 13], 12, -40341101);
-        c = md5ff(c, d, a, b, x[i + 14], 17, -1502002290);
-        b = md5ff(b, c, d, a, x[i + 15], 22, 1236535329);
+        a = FF(a, b, c, d, x[i], 7, -680876936);
+        d = FF(d, a, b, c, x[i + 1], 12, -389564586);
+        c = FF(c, d, a, b, x[i + 2], 17, 606105819);
+        b = FF(b, c, d, a, x[i + 3], 22, -1044525330);
+        a = FF(a, b, c, d, x[i + 4], 7, -176418897);
+        d = FF(d, a, b, c, x[i + 5], 12, 1200080426);
+        c = FF(c, d, a, b, x[i + 6], 17, -1473231341);
+        b = FF(b, c, d, a, x[i + 7], 22, -45705983);
+        a = FF(a, b, c, d, x[i + 8], 7, 1770035416);
+        d = FF(d, a, b, c, x[i + 9], 12, -1958414417);
+        c = FF(c, d, a, b, x[i + 10], 17, -42063);
+        b = FF(b, c, d, a, x[i + 11], 22, -1990404162);
+        a = FF(a, b, c, d, x[i + 12], 7, 1804603682);
+        d = FF(d, a, b, c, x[i + 13], 12, -40341101);
+        c = FF(c, d, a, b, x[i + 14], 17, -1502002290);
+        b = FF(b, c, d, a, x[i + 15], 22, 1236535329);
         /* Round 2 */
-        a = md5gg(a, b, c, d, x[i + 1], 5, -165796510);
-        d = md5gg(d, a, b, c, x[i + 6], 9, -1069501632);
-        c = md5gg(c, d, a, b, x[i + 11], 14, 643717713);
-        b = md5gg(b, c, d, a, x[i], 20, -373897302);
-        a = md5gg(a, b, c, d, x[i + 5], 5, -701558691);
-        d = md5gg(d, a, b, c, x[i + 10], 9, 38016083);
-        c = md5gg(c, d, a, b, x[i + 15], 14, -660478335);
-        b = md5gg(b, c, d, a, x[i + 4], 20, -405537848);
-        a = md5gg(a, b, c, d, x[i + 9], 5, 568446438);
-        d = md5gg(d, a, b, c, x[i + 14], 9, -1019803690);
-        c = md5gg(c, d, a, b, x[i + 3], 14, -187363961);
-        b = md5gg(b, c, d, a, x[i + 8], 20, 1163531501);
-        a = md5gg(a, b, c, d, x[i + 13], 5, -1444681467);
-        d = md5gg(d, a, b, c, x[i + 2], 9, -51403784);
-        c = md5gg(c, d, a, b, x[i + 7], 14, 1735328473);
-        b = md5gg(b, c, d, a, x[i + 12], 20, -1926607734);
+        a = GG(a, b, c, d, x[i + 1], 5, -165796510);
+        d = GG(d, a, b, c, x[i + 6], 9, -1069501632);
+        c = GG(c, d, a, b, x[i + 11], 14, 643717713);
+        b = GG(b, c, d, a, x[i], 20, -373897302);
+        a = GG(a, b, c, d, x[i + 5], 5, -701558691);
+        d = GG(d, a, b, c, x[i + 10], 9, 38016083);
+        c = GG(c, d, a, b, x[i + 15], 14, -660478335);
+        b = GG(b, c, d, a, x[i + 4], 20, -405537848);
+        a = GG(a, b, c, d, x[i + 9], 5, 568446438);
+        d = GG(d, a, b, c, x[i + 14], 9, -1019803690);
+        c = GG(c, d, a, b, x[i + 3], 14, -187363961);
+        b = GG(b, c, d, a, x[i + 8], 20, 1163531501);
+        a = GG(a, b, c, d, x[i + 13], 5, -1444681467);
+        d = GG(d, a, b, c, x[i + 2], 9, -51403784);
+        c = GG(c, d, a, b, x[i + 7], 14, 1735328473);
+        b = GG(b, c, d, a, x[i + 12], 20, -1926607734);
         /* Round 3 */
-        a = md5hh(a, b, c, d, x[i + 5], 4, -378558);
-        d = md5hh(d, a, b, c, x[i + 8], 11, -2022574463);
-        c = md5hh(c, d, a, b, x[i + 11], 16, 1839030562);
-        b = md5hh(b, c, d, a, x[i + 14], 23, -35309556);
-        a = md5hh(a, b, c, d, x[i + 1], 4, -1530992060);
-        d = md5hh(d, a, b, c, x[i + 4], 11, 1272893353);
-        c = md5hh(c, d, a, b, x[i + 7], 16, -155497632);
-        b = md5hh(b, c, d, a, x[i + 10], 23, -1094730640);
-        a = md5hh(a, b, c, d, x[i + 13], 4, 681279174);
-        d = md5hh(d, a, b, c, x[i], 11, -358537222);
-        c = md5hh(c, d, a, b, x[i + 3], 16, -722521979);
-        b = md5hh(b, c, d, a, x[i + 6], 23, 76029189);
-        a = md5hh(a, b, c, d, x[i + 9], 4, -640364487);
-        d = md5hh(d, a, b, c, x[i + 12], 11, -421815835);
-        c = md5hh(c, d, a, b, x[i + 15], 16, 530742520);
-        b = md5hh(b, c, d, a, x[i + 2], 23, -995338651);
+        a = HH(a, b, c, d, x[i + 5], 4, -378558);
+        d = HH(d, a, b, c, x[i + 8], 11, -2022574463);
+        c = HH(c, d, a, b, x[i + 11], 16, 1839030562);
+        b = HH(b, c, d, a, x[i + 14], 23, -35309556);
+        a = HH(a, b, c, d, x[i + 1], 4, -1530992060);
+        d = HH(d, a, b, c, x[i + 4], 11, 1272893353);
+        c = HH(c, d, a, b, x[i + 7], 16, -155497632);
+        b = HH(b, c, d, a, x[i + 10], 23, -1094730640);
+        a = HH(a, b, c, d, x[i + 13], 4, 681279174);
+        d = HH(d, a, b, c, x[i], 11, -358537222);
+        c = HH(c, d, a, b, x[i + 3], 16, -722521979);
+        b = HH(b, c, d, a, x[i + 6], 23, 76029189);
+        a = HH(a, b, c, d, x[i + 9], 4, -640364487);
+        d = HH(d, a, b, c, x[i + 12], 11, -421815835);
+        c = HH(c, d, a, b, x[i + 15], 16, 530742520);
+        b = HH(b, c, d, a, x[i + 2], 23, -995338651);
         /* Round 4 */
-        a = md5ii(a, b, c, d, x[i], 6, -198630844);
-        d = md5ii(d, a, b, c, x[i + 7], 10, 1126891415);
-        c = md5ii(c, d, a, b, x[i + 14], 15, -1416354905);
-        b = md5ii(b, c, d, a, x[i + 5], 21, -57434055);
-        a = md5ii(a, b, c, d, x[i + 12], 6, 1700485571);
-        d = md5ii(d, a, b, c, x[i + 3], 10, -1894986606);
-        c = md5ii(c, d, a, b, x[i + 10], 15, -1051523);
-        b = md5ii(b, c, d, a, x[i + 1], 21, -2054922799);
-        a = md5ii(a, b, c, d, x[i + 8], 6, 1873313359);
-        d = md5ii(d, a, b, c, x[i + 15], 10, -30611744);
-        c = md5ii(c, d, a, b, x[i + 6], 15, -1560198380);
-        b = md5ii(b, c, d, a, x[i + 13], 21, 1309151649);
-        a = md5ii(a, b, c, d, x[i + 4], 6, -145523070);
-        d = md5ii(d, a, b, c, x[i + 11], 10, -1120210379);
-        c = md5ii(c, d, a, b, x[i + 2], 15, 718787259);
-        b = md5ii(b, c, d, a, x[i + 9], 21, -343485551);
+        a = II(a, b, c, d, x[i], 6, -198630844);
+        d = II(d, a, b, c, x[i + 7], 10, 1126891415);
+        c = II(c, d, a, b, x[i + 14], 15, -1416354905);
+        b = II(b, c, d, a, x[i + 5], 21, -57434055);
+        a = II(a, b, c, d, x[i + 12], 6, 1700485571);
+        d = II(d, a, b, c, x[i + 3], 10, -1894986606);
+        c = II(c, d, a, b, x[i + 10], 15, -1051523);
+        b = II(b, c, d, a, x[i + 1], 21, -2054922799);
+        a = II(a, b, c, d, x[i + 8], 6, 1873313359);
+        d = II(d, a, b, c, x[i + 15], 10, -30611744);
+        c = II(c, d, a, b, x[i + 6], 15, -1560198380);
+        b = II(b, c, d, a, x[i + 13], 21, 1309151649);
+        a = II(a, b, c, d, x[i + 4], 6, -145523070);
+        d = II(d, a, b, c, x[i + 11], 10, -1120210379);
+        c = II(c, d, a, b, x[i + 2], 15, 718787259);
+        b = II(b, c, d, a, x[i + 9], 21, -343485551);
         /* Round Last */
         a = safeAdd(a, originA);
         b = safeAdd(b, originB);
