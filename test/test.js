33119f1d678423b15db1ef9976118c2e4e6d1629
huiyifyj <jxfengyijie@gmail.com>
2020-03-20 19:38:43 +0800

    Test special reserved characters of URI
    - Add special string test:
      Test reserved characters of URI: ';' '/' '?' ':' '@' '&' '=' '+' '$' ','.
      More informations to [URI Handling Function Properties](https://ecma-international.org/ecma-262/5.1/#sec-15.1.3).
    - Update annotation.
    
    Note: correlative hex-hash value of uriReserved is generated by https://play.golang.org/p/t0lDH-X2Fpg.

--- test/test.js
+++ test/test.js
@@ -5,18 +5,20 @@ const assert = require('assert');
 
 const md5 = require('../md5');
 
-// Instance Object for Testing
+// Instance object for testing
 const example = {
     name: 'xn-02f',
     email: 'i@huiyifyj.cn',
-    url: 'xn--02f.com'
+    url: 'xn--02f.com',
+    uriReserved: ';/?:@&=+$,'
 }
 
-// Related MD5 Hash Value
+// Related MD5 hash value
 const md5Hash = {
     name: '54d30fa674d13e3598970bc9c5e2388e',
     email: '11b334f003ef029c9d154f5dbae18b44',
-    url: '014dab5b5a990d379b2306f5d0839261'
+    url: '014dab5b5a990d379b2306f5d0839261',
+    uriReserved: 'cae1061daebd7e3700817d67a2727fc2'
 }
 
 describe('MD5 Test', () => {
@@ -30,5 +32,11 @@ describe('MD5 Test', () => {
 
     it('"' + example.url + '" Converted to MD5 Value Test.', () => {
         assert.equal(md5(example.url), md5Hash.url);
+    });
+});
+
+describe('Special Strings Test', () => {
+    it('Test reserved characters of URI: "' + example.uriReserved + '".', () => {
+        assert.equal(md5(example.uriReserved), md5Hash.uriReserved);
     });
 });
