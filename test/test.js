d20f6eb659f3b617e02d03ad25beb9d3c1a4fec5
huiyifyj <jxfengyijie@gmail.com>
2020-03-21 13:42:08 +0800

    Add chinese string test
    Add chinese string test.
    And its hex-hash value is generated by https://play.golang.org/p/zAhNV2NVFl5.

--- test/test.js
+++ test/test.js
@@ -10,6 +10,7 @@ const example = {
     name: 'xn-02f',
     email: 'i@huiyifyj.cn',
     url: 'xn--02f.com',
+    chinese: '中文',
     uriReserved: ';/?:@&=+$,'
 }
 
@@ -18,6 +19,7 @@ const md5Hash = {
     name: '54d30fa674d13e3598970bc9c5e2388e',
     email: '11b334f003ef029c9d154f5dbae18b44',
     url: '014dab5b5a990d379b2306f5d0839261',
+    chinese: 'a7bac2239fcdcb3a067903d8077c4a07',
     uriReserved: 'cae1061daebd7e3700817d67a2727fc2'
 }
 
@@ -38,5 +40,9 @@ describe('MD5 Test', () => {
 describe('Special Strings Test', () => {
     it('Test reserved characters of URI: "' + example.uriReserved + '".', () => {
         assert.equal(md5(example.uriReserved), md5Hash.uriReserved);
+    });
+
+    it('Test chinese string "' + example.chinese + '".', () => {
+        assert.equal(md5(example.chinese), md5Hash.chinese);
     });
 });
