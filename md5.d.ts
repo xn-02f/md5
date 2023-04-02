daee30d5da4148cfb5ae0160b0fccbe06ca63520
huiyifyj <jxfengyijie@gmail.com>
2023-04-02 16:28:30 +0800

    Fix JSDoc doesn't work and bump version to v2.4.2
    - Bump version to v2.4.2.
    - Fix JSDoc doesn't work by commenting in the correct place.

--- md5.d.ts
+++ md5.d.ts
@@ -4,6 +4,6 @@
  * @param {string} data - The raw string you want to convert.
  * @return {string} The hexadecimal hash string that is handled and converted.
  */
-export = md5;
-
 declare const md5: (data: string) => string;
+
+export = md5;
